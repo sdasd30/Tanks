203b911f89e0307a38aafb2e59d4b67be864a410
sdasd30 <danielzhao2@gmail.com>
2020-08-01 00:33:17 -0400

    Added A* package and new MovementRAW script

--- Assets/Scripts/Movement/Player/MovementBodyRaw.cs
+++ Assets/Scripts/Movement/Player/MovementBodyRaw.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class MovementBodyRaw : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rgd;
+    PlayerInputBody playerInput;
+    public float MoveSpeed;
+    public bool playerControl;
+    InputPacket ip;
     void Start()
     {
-        
+        rgd = GetComponent<Rigidbody2D>();
+        ip = new InputPacket();
+        if (playerControl)
+            playerInput = GetComponent<PlayerInputBody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        if (playerControl)
+            ip = playerInput.getInputPacket(ip);
+        Move(ip);
+
+    }
+
+    public void Move(InputPacket ip)
+    {
+        rgd.position = new Vector2(rgd.position.x - ip.inputRotate * MoveSpeed * Time.fixedDeltaTime, rgd.position.y + ip.inputTranslate * MoveSpeed * Time.fixedDeltaTime);
     }
 }
